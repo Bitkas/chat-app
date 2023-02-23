ca41c4447da0981dfa82cdbe5a1e298a63697914
Bitkas <bitokeiro@gmail.com>
2023-02-23 10:45:34 +0000

    Add Firebase and Firestore

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,16 @@
 import express, {Response, Request, Express} from 'express';
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
+
+import { environment } from './environment';
+import { firebaseService } from './firebase.service';
 const app: Express = express();
 const PORT: number = 3000;
 
 app.use(express.static("../angular-app/dist/chat-app"));
+
+initializeApp(environment.firebase);
+const firestoreService = new firebaseService(getFirestore());;
 
 app.get("/api/data", (req: Request, res: Response) => {
   res.status(200).json({data: "Hello World!"})  
