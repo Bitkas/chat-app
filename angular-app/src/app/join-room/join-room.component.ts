88540e4b448a247ce186cc7a3ac00c534838a936
Bitkas <bitokeiro@gmail.com>
2023-02-25 15:38:16 +0000

    format files

--- angular-app/src/app/join-room/join-room.component.ts
+++ angular-app/src/app/join-room/join-room.component.ts
@@ -4,26 +4,29 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-join-room',
   templateUrl: './join-room.component.html',
-  styleUrls: ['./join-room.component.css']
+  styleUrls: ['./join-room.component.css'],
 })
 export class JoinRoomComponent {
   rooms: Array<string> = [];
-  @Output() joinRoomEvent = new EventEmitter<{isRoomChosen: boolean, room: string}>();
+  @Output() joinRoomEvent = new EventEmitter<{
+    isRoomChosen: boolean;
+    room: string;
+  }>();
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    const request = this.http.get<Array<string>>("/rooms/list");
+    const request = this.http.get<Array<string>>('/rooms/list');
     request.subscribe((data: Array<string>) => {
       this.rooms = data;
-    })
+    });
   }
 
   JoinRoom(room: string) {
-    console.log("ROOM: ", room)
+    console.log('ROOM: ', room);
     const value = {
       room: room,
-      isRoomChosen: true
+      isRoomChosen: true,
     };
     this.joinRoomEvent.emit(value);
   }
