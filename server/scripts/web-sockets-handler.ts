91d34f6ac30fd06752b64d5a7db61d0686645215
Bitkas <bitokeiro@gmail.com>
2023-02-28 10:16:55 +0000

    Format files

--- server/scripts/web-sockets-handler.ts
+++ server/scripts/web-sockets-handler.ts
@@ -2,28 +2,25 @@ import { Server, Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
 export class webSocketHandler {
+  private socket: Socket;
+  constructor(socket: Socket) {
+    this.socket = socket;
+    socket.on("joinRoom", this.JoinRoom.bind(this));
+    socket.on("message", this.broadcastMessage.bind(this));
+    socket.on("disconnect", (reason) => {
+      console.log("DISCONNECTED, REASON: ", reason);
+    });
+  }
 
-    private socket: Socket;
-    constructor(socket: Socket) {
-        this.socket = socket;
-        socket.on("joinRoom", this.JoinRoom.bind(this));
-  socket.on("message", this.BroadcastMessage.bind(this));
-  socket.on("disconnect", (reason) => {
-    console.log("DISCONNECTED, REASON: ", reason);
-  })
-        this.JoinRoom = this.JoinRoom.bind(this);
-        this.BroadcastMessage = this.BroadcastMessage.bind(this);
-    }
+  JoinRoom(room: string): void {
+    this.socket.join(room);
+  }
 
-    JoinRoom(room: string): void {
-        this.socket.join(room);
-    }
-    
-    BroadcastMessage(message: string, room: string): void {
-        this.socket.broadcast.to(room).emit("broadcast-message", message)
-    }
+  broadcastMessage(message: string, room: string): void {
+    this.socket.broadcast.to(room).emit("broadcast-message", message);
+  }
 
-    get getSocket(): Socket {
-        return this.socket;
-    }
+  get getSocket(): Socket {
+    return this.socket;
+  }
 }
