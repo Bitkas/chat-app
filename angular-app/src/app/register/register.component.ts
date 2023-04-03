c8e8f679ea3ee9530a30b1cb543f7eea4194af00
Bitkas <bitokeiro@gmail.com>
2023-04-03 13:25:31 +0100

    Add register logic

--- angular-app/src/app/register/register.component.ts
+++ angular-app/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  register(username: string, password: string): void {
+    const user = {
+      "username": username,
+      "password": password
+    };
+
+    const req = this.http.post("/register", user);
+
+    req.subscribe((res) => {
+      console.log("user created: ", res);
+      this.router.navigate(["login"]);
+    })
+  }
 }
