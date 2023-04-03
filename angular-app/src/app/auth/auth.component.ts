c98ea77b7e3850e7c1d319a028669b08ff9c6a97
Bitkas <bitokeiro@gmail.com>
2023-04-03 13:25:54 +0100

    Add login logic

--- angular-app/src/app/auth/auth.component.ts
+++ angular-app/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  constructor(private http: HttpClient, private router: Router) {}
 
+  authenticate(username: string, password: string): void {
+    const user = {
+      "username": username,
+      "password": password
+    };
+
+    const req = this.http.post("/login", user);
+
+    req.subscribe((res) => {
+      console.log("LOGIN: ", res);
+      this.router.navigate(["createRoom"]);
+
+    })
+  }
 }
